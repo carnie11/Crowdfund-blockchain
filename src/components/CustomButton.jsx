74302c14c7935df54b60dc3a363b5fe08bd4e58f
carnie11 <carolin.modigh@gmail.com>
2023-01-11 09:25:31 +0100

    Initial commit

---
+++ src/components/CustomButton.jsx
@@ -0,0 +1,18 @@
+import { title } from 'process';
+import React from 'react';
+
+
+const CustomButton = ({ btnType, title, handleClick, styles }) => {
+    return (
+        <button
+            type={btnType}
+            className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4rounded-[10px] ${styles}`}
+            onClick={handleClick}
+        >
+            {title}
+
+        </button>
+    )
+}
+
+export default CustomButton
